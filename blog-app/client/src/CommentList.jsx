b966db8c8f10bc5bd4c187bf5d67c0434386ac94
sohaibsalman <sohaibsalman10@gmail.com>
2022-05-09 13:58:08 +0500

    Rendering comments by status

--- blog-app/client/src/CommentList.jsx
+++ blog-app/client/src/CommentList.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 const CommentList = ({ comments }) => {
     const renderedComments = comments.map((comment) => {
+        let content;
+
+        if (comment.status === 'approved') {
+            content = comment.content;
+        } else if (comment.status === 'rejected') {
+            content = 'This comment has been rejected';
+        } else if (comment.status === 'pending') {
+            content = 'This comment is awaiting moderation';
+        }
+
         return (
             <li className="list-group-item" key={comment.id}>
-                {comment.content}
+                {content}
             </li>
         );
     });
