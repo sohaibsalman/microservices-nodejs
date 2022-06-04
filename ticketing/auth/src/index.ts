fed5e1567d977072451ef8650504f0dfa02e9128
sohaibsalman <sohaibsalman10@gmail.com>
2022-06-04 20:25:38 +0500

    Add not found error class

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(express.json());
@@ -14,6 +16,11 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
 app.listen(3000, () => {
