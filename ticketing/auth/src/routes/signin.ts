7fa2263bec476ecc74105bbd360e53991e5121ce
sohaibsalman <sohaibsalman10@gmail.com>
2022-06-10 21:21:51 +0500

    Common Request validation middleware

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,7 +1,18 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email is invalid'),
+    body('password').notEmpty().withMessage('You must enter a passowrd'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {}
+);
 
 export { router as signinRouter };
